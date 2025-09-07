f2b0ae08dfc8322c2b9de76119cb6526a415d680
Matt Martz <matt.martz@gmail.com>
2025-09-07 16:51:52 -0400

    Initial commit

---
+++ test/mcp-cdk-lambda-cognito.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as McpCdkLambdaCognito from '../lib/mcp-cdk-lambda-cognito-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/mcp-cdk-lambda-cognito-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new McpCdkLambdaCognito.McpCdkLambdaCognitoStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
